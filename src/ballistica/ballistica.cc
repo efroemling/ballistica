cbebdfe285e89446e96ef5bfcaa229817d2c34e4
Eric Froemling <ericfroemling@gmail.com>
2020-10-30 16:24:25 -0500

    Attempting to improve compatibility with Windows7

--- src/ballistica/ballistica.cc
+++ src/ballistica/ballistica.cc
@@ -21,7 +21,7 @@
 namespace ballistica {
 
 // These are set automatically via script; don't change here.
-const int kAppBuildNumber = 20234;
+const int kAppBuildNumber = 20235;
 const char* kAppVersion = "1.5.27";
 
 // Our standalone globals.
