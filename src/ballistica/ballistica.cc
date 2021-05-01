2ef46743cf042a13639691afa00e59966832354f
Eric Froemling <ericfroemling@gmail.com>
2021-04-30 22:19:33 -0500

    tidying and language updates

--- src/ballistica/ballistica.cc
+++ src/ballistica/ballistica.cc
@@ -21,7 +21,7 @@
 namespace ballistica {
 
 // These are set automatically via script; don't change here.
-const int kAppBuildNumber = 20353;
+const int kAppBuildNumber = 20355;
 const char* kAppVersion = "1.6.0";
 
 // Our standalone globals.
