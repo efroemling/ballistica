6efedaefa7a9e92417e8beff6b601b38f4777b2d
Eric Froemling <ericfroemling@gmail.com>
2021-06-03 12:23:42 -0500

    More work on win compiles

---
+++ src/ballistica/config/config_windows_headless.h
@@ -0,0 +1,18 @@
+// Released under the MIT License. See LICENSE for details.
+
+#ifndef BALLISTICA_CONFIG_CONFIG_WINDOWS_HEADLESS_H_
+#define BALLISTICA_CONFIG_CONFIG_WINDOWS_HEADLESS_H_
+
+// note: define overrides BEFORE common header
+#define BA_HEADLESS_BUILD 1
+
+#define BA_USE_STDIN_THREAD 1
+
+#define BA_MINSDL_BUILD 1
+
+#include "ballistica/config/config_windows_common.h"
+
+// This must always be last.
+#include "ballistica/config/config_common.h"
+
+#endif  // BALLISTICA_CONFIG_CONFIG_WINDOWS_HEADLESS_H_
