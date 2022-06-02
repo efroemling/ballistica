1d0c6439d4d18f4eee070842ac5b3bc3b7ae1a69
Drooopyyy <90500887+Drooopyyy@users.noreply.github.com>
2022-06-02 12:56:06 +0530

    Update capturetheflag.py

--- assets/src/ba_data/python/bastd/game/capturetheflag.py
+++ assets/src/ba_data/python/bastd/game/capturetheflag.py
@@ -274,6 +274,10 @@ class CaptureTheFlagGame(ba.TeamGameActivity[Player, Team]):
 
             # If the enemy flag is already here, score!
             if team.enemy_flag_at_base:
+                self.show_zoom_message(
+                        ba.Lstr(resource='nameScoresText',
+                             subs=[('${NAME}', team.name.evaluate())]),
+                        color=team.color)  
                 self._score(team)
         else:
             team.enemy_flag_at_base = True
