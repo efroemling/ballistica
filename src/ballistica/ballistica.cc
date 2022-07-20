32cb0600917443828d7bf8fab6b5b0394c097c7a
Eric Froemling <ericfroemling@gmail.com>
2022-07-20 11:29:28 -0700

    fixed version pcommand not being available in public

--- src/ballistica/ballistica.cc
+++ src/ballistica/ballistica.cc
@@ -21,7 +21,7 @@
 namespace ballistica {
 
 // These are set automatically via script; don't modify them here.
-const int kAppBuildNumber = 20660;
+const int kAppBuildNumber = 20661;
 const char* kAppVersion = "1.7.5";
 
 // Our standalone globals.
