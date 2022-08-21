7021677941dfb064717086231c8231be0d3eb84f
Eric Froemling <ericfroemling@gmail.com>
2022-08-21 10:48:28 -0700

    tidying

--- src/ballistica/ballistica.cc
+++ src/ballistica/ballistica.cc
@@ -21,7 +21,7 @@
 namespace ballistica {
 
 // These are set automatically via script; don't modify them here.
-const int kAppBuildNumber = 20692;
+const int kAppBuildNumber = 20694;
 const char* kAppVersion = "1.7.7";
 
 // Our standalone globals.
