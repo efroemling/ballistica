9e1d6510272a99d25fc29ac2f1355f37844b0de6
Eric <ericfroemling@gmail.com>
2022-09-05 10:01:43 -0700

    added generic copied-to-clipboard translation entry

--- src/ballistica/ballistica.cc
+++ src/ballistica/ballistica.cc
@@ -22,7 +22,7 @@
 namespace ballistica {
 
 // These are set automatically via script; don't modify them here.
-const int kAppBuildNumber = 20764;
+const int kAppBuildNumber = 20766;
 const char* kAppVersion = "1.7.7";
 
 // Our standalone globals.
