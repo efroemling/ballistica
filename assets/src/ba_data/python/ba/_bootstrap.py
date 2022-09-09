5ccaad64d60beb8386386323942125a842fe84da
Eric <ericfroemling@gmail.com>
2022-09-09 15:16:56 -0700

    changelog note on module removal

--- assets/src/ba_data/python/ba/_bootstrap.py
+++ assets/src/ba_data/python/ba/_bootstrap.py
@@ -38,7 +38,7 @@ def bootstrap() -> None:
 
     # Give a soft warning if we're being used with a different binary
     # version than we expect.
-    expected_build = 20794
+    expected_build = 20796
     running_build: int = env['build_number']
     if running_build != expected_build:
         print(
