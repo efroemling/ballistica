92a8ca40b34cfb75ac9fe7cba07e9bffd9565ecd
Eric <ericfroemling@gmail.com>
2022-09-17 23:07:58 -0700

    tidying

--- assets/src/ba_data/python/ba/_bootstrap.py
+++ assets/src/ba_data/python/ba/_bootstrap.py
@@ -44,7 +44,7 @@ def bootstrap() -> None:
 
     # Give a soft warning if we're being used with a different binary
     # version than we expect.
-    expected_build = 20860
+    expected_build = 20861
     running_build: int = env['build_number']
     if running_build != expected_build:
         print(
