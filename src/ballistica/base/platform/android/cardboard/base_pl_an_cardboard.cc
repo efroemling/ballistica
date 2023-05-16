1f2aca9f699b201ef1baa46d10a5f78732dae040
Eric Froemling <ericfroemling@gmail.com>
2023-05-16 12:51:16 -0700

    2ci2furious

--- src/ballistica/base/platform/android/cardboard/base_pl_an_cardboard.cc
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2011-2022 Eric Froemling
-
-#if BA_CARDBOARD_BUILD
-
-#include "ballistica/base/platform/android/cardboard/base_pl_an_cardboard.h"
-
-namespace ballistica::base {}  // namespace ballistica::base
-
-#endif  // BA_CARDBOARD_BUILD
