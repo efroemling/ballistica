fa8600304a8ac8b94280731ad67f48f567a219a9
Eric <ericfroemling@gmail.com>
2023-06-12 14:43:38 -0700

    formalizing system to build & run the app as part of tool commands

--- src/ballistica/shared/ballistica.cc
+++ src/ballistica/shared/ballistica.cc
@@ -39,7 +39,7 @@ auto main(int argc, char** argv) -> int {
 namespace ballistica {
 
 // These are set automatically via script; don't modify them here.
-const int kEngineBuildNumber = 21091;
+const int kEngineBuildNumber = 21092;
 const char* kEngineVersion = "1.7.20";
 
 auto MonolithicMain(const core::CoreConfig& core_config) -> int {
