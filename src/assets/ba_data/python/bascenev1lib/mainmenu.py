30d6a3ed6558c55d25276339bc281d67648f13c4
Vishal <vishal.u338@gmail.com>
2023-11-12 09:06:18 +0530

    Update mainmenu.py

--- src/assets/ba_data/python/bascenev1lib/mainmenu.py
+++ src/assets/ba_data/python/bascenev1lib/mainmenu.py
@@ -83,7 +83,7 @@ class MainMenuActivity(bs.Activity[bs.Player, bs.Team]):
                         'scale': scale,
                         'position': (0, 10),
                         'vr_depth': -10,
-                        'text': self.get_year_and_name(),
+                        'text': bs.WeakCall(self.get_year_and_name()),
                     },
                 )
             )
@@ -826,15 +826,13 @@ class MainMenuActivity(bs.Activity[bs.Player, bs.Team]):
 
         bui.apptimer(0.5, _start_menu_music)
 
-    def get_year_and_name(self):
+    def get_year_and_name(self) -> str:
         from urllib.request import urlopen, URLError
 
         year = 2023
         try:
-            response = urlopen('https://www.ballistica.net', timeout = 5)
-
-            if response.getcode() == 200:
-                year = time.strftime('%Y', time.gmtime())
+            urlopen('https://www.ballistica.net', timeout = 5)
+            year = time.strftime('%Y', time.gmtime())
         except URLError:
             pass
         name = f'\xa9 2011-{year} Eric Froemling'
