388fc69ae7197a08a86341d6c8d61e4b7363bcb1
Vishal <vishal.u338@gmail.com>
2025-01-19 15:23:22 +0530

    Bringing back the bomb spinner

--- src/ballistica/ui_v1/widget/spinner_widget.h
+++ src/ballistica/ui_v1/widget/spinner_widget.h
@@ -34,7 +34,7 @@ class SpinnerWidget : public Widget {
  private:
   float size_{32.0f};
   float presence_{};
-  Style style_{Style::kSimple};
+  Style style_{Style::kBomb};
   bool visible_{true};
 };
 
