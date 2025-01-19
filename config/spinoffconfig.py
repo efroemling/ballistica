69c0e15c625ef189b181f9e82a89b461b9fac5e4
era <e@4d2.org>
2025-01-19 16:52:21 +0330

    im an idiot

--- config/spinoffconfig.py
+++ config/spinoffconfig.py
@@ -202,11 +202,11 @@ ctx.filter_file_names = {
     'ballistica_dev_image.yaml',
     'ci_check_linux.yaml',
     'ci_build_and_test_linux.yaml',
-    'ci_spinoff_test_linux.yaml'
+    'ci_spinoff_test_linux.yaml',
     'ci_feature_set_copy_test_linux.yaml',
     'ci_build_and_test_windows.yaml',
-    'deploy_docs.yaml'
-    'release.yaml'
+    'deploy_docs.yaml',
+    'release.yaml',
     'LICENSE',
     'cloudtool',
     'bacloud',
