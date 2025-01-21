81339dd31b074175e114c5b0cf2ad38f389a343a
Eric Froemling <ericfroemling@gmail.com>
2025-01-20 21:48:07 -0800

    advanced settings now adapts to screen size

--- src/assets/ba_data/python/bauiv1lib/settings/advanced.py
+++ src/assets/ba_data/python/bauiv1lib/settings/advanced.py
@@ -39,34 +39,51 @@ class AdvancedSettingsWindow(bui.MainWindow):
 
         uiscale = bui.app.ui_v1.uiscale
         self._width = 1030.0 if uiscale is bui.UIScale.SMALL else 670.0
-        x_inset = 150 if uiscale is bui.UIScale.SMALL else 0
         self._height = (
-            390.0
+            490.0
             if uiscale is bui.UIScale.SMALL
-            else 450.0 if uiscale is bui.UIScale.MEDIUM else 520.0
+            else 450.0 if uiscale is bui.UIScale.MEDIUM else 550.0
         )
         self._lang_status_text: bui.Widget | None = None
 
         self._spacing = 32
         self._menu_open = False
-        top_extra = 10 if uiscale is bui.UIScale.SMALL else 0
+
+        # Do some fancy math to fill all available screen area up to the
+        # size of our backing container.
+        #
+        # TODO: We need an auto-refresh mechanism for cases where screen
+        # size changes under us. Currently one must navigate out and
+        # back in to properly reflect such changes.
+        screensize = bui.get_virtual_screen_size()
+        scale = (
+            2.04
+            if uiscale is bui.UIScale.SMALL
+            else 1.3 if uiscale is bui.UIScale.MEDIUM else 1.0
+        )
+
+        # Calc screen size in our local container space and clamp to a
+        # bit smaller than our container size.
+        target_width = min(self._width - 80, screensize[0] / scale)
+        target_height = min(self._height - 80, screensize[1] / scale)
+
+        # To get top/left coords, go to the center of our window and
+        # offset by half the width/height of our target area.
+        yoffs = 0.5 * self._height + 0.5 * target_height + 30.0
+
+        self._scroll_width = target_width
+        self._scroll_height = target_height - 25
+        scroll_y = yoffs - 56 - self._scroll_height
 
         super().__init__(
             root_widget=bui.containerwidget(
-                size=(self._width, self._height + top_extra),
+                size=(self._width, self._height),
                 toolbar_visibility=(
                     'menu_minimal'
                     if uiscale is bui.UIScale.SMALL
                     else 'menu_full'
                 ),
-                scale=(
-                    2.04
-                    if uiscale is bui.UIScale.SMALL
-                    else 1.4 if uiscale is bui.UIScale.MEDIUM else 1.0
-                ),
-                stack_offset=(
-                    (0, 10) if uiscale is bui.UIScale.SMALL else (0, 0)
-                ),
+                scale=scale,
             ),
             transition=transition,
             origin_widget=origin_widget,
@@ -82,11 +99,7 @@ class AdvancedSettingsWindow(bui.MainWindow):
         # so no need to show this.
         self._show_always_use_internal_keyboard = not app.env.vr
 
-        self._scroll_width = self._width - (100 + 2 * x_inset)
-        self._scroll_height = self._height - (
-            125.0 if uiscale is bui.UIScale.SMALL else 115.0
-        )
-        self._sub_width = self._scroll_width * 0.95
+        self._sub_width = min(550, self._scroll_width * 0.95)
         self._sub_height = 870.0
 
         if self._show_always_use_internal_keyboard:
@@ -121,12 +134,12 @@ class AdvancedSettingsWindow(bui.MainWindow):
         else:
             self._back_button = bui.buttonwidget(
                 parent=self._root_widget,
-                position=(53 + x_inset, self._height - 60),
-                size=(140, 60),
+                position=(50, yoffs - 48),
+                size=(60, 60),
                 scale=0.8,
                 autoselect=True,
-                label=bui.Lstr(resource='backText'),
-                button_type='back',
+                label=bui.charstr(bui.SpecialChar.BACK),
+                button_type='backSmall',
                 on_activate_call=self.main_window_back,
             )
             bui.containerwidget(
@@ -137,30 +150,23 @@ class AdvancedSettingsWindow(bui.MainWindow):
             parent=self._root_widget,
             position=(
                 self._width * 0.5,
-                self._height - (61 if uiscale is bui.UIScale.SMALL else 40),
+                yoffs - (43 if uiscale is bui.UIScale.SMALL else 25),
             ),
             size=(0, 0),
-            scale=0.85 if uiscale is bui.UIScale.SMALL else 1.0,
+            scale=0.75 if uiscale is bui.UIScale.SMALL else 1.0,
             text=bui.Lstr(resource=f'{self._r}.titleText'),
             color=app.ui_v1.title_color,
             h_align='center',
             v_align='center',
         )
 
-        if self._back_button is not None:
-            bui.buttonwidget(
-                edit=self._back_button,
-                button_type='backSmall',
-                size=(60, 60),
-                label=bui.charstr(bui.SpecialChar.BACK),
-            )
-
         self._scrollwidget = bui.scrollwidget(
             parent=self._root_widget,
-            position=(50 + x_inset, 50),
+            size=(self._scroll_width, self._scroll_height),
+            position=(self._width * 0.5 - self._scroll_width * 0.5, scroll_y),
             simple_culling_v=20.0,
             highlight=False,
-            size=(self._scroll_width, self._scroll_height),
+            center_small_content_horizontally=True,
             selection_loops_to_parent=True,
             border_opacity=0.4,
         )
