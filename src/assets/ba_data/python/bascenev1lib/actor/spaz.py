6fca3a9ac864b5346474536a011da3f44ed22276
Vishal <vishal.u338@gmail.com>
2025-02-23 01:48:46 +0530

    Fixing CI

--- src/assets/ba_data/python/bascenev1lib/actor/spaz.py
+++ src/assets/ba_data/python/bascenev1lib/actor/spaz.py
@@ -886,7 +886,10 @@ class Spaz(bs.Actor):
             if not self.frozen:
                 self.frozen = True
                 self.node.frozen = True
-                bs.timer(msg.time, bs.WeakCall(self.handlemessage, bs.ThawMessage()))
+                bs.timer(
+                    msg.time,
+                    bs.WeakCall(self.handlemessage, bs.ThawMessage())
+                )
                 # Instantly shatter if we're already dead.
                 # (otherwise its hard to tell we're dead).
                 if self.hitpoints <= 0:
