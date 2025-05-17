9cffbff78bc84cb8dd71cc883a968723a235056e
Loup-Garou911XD <90267658+Loup-Garou911XD@users.noreply.github.com>
2025-05-17 16:51:25 +0530

    storing token in file

--- src/ballistica/base/discord/discord.cc
+++ src/ballistica/base/discord/discord.cc
@@ -3,6 +3,8 @@
 #define DISCORDPP_IMPLEMENTATION
 #include "discord.h"
 
+#include <bits/stdc++.h>
+
 #include <atomic>
 #include <csignal>
 #include <cstdio>
@@ -34,22 +36,22 @@ void DiscordClient::init() {
       },
       discordpp::LoggingSeverity::Info);
 
-  client->SetStatusChangedCallback([this,client](discordpp::Client::Status status,
-                                            discordpp::Client::Error error,
-                                            int32_t errorDetail) {
-    std::cout << "🔄 Status changed: "
-              << discordpp::Client::StatusToString(status) << std::endl;
+  client->SetStatusChangedCallback(
+      [this, client](discordpp::Client::Status status,
+                     discordpp::Client::Error error, int32_t errorDetail) {
+        std::cout << "🔄 Status changed: "
+                  << discordpp::Client::StatusToString(status) << std::endl;
 
-    if (status == discordpp::Client::Status::Ready) {
-      std::cout << "✅ Client is ready! You can now call SDK functions.\n";
-      SetActivity(client, "Playing", "Ballistica");
+        if (status == discordpp::Client::Status::Ready) {
+          std::cout << "✅ Client is ready! You can now call SDK functions.\n";
+          SetActivity(client, "Playing", "Ballistica");
 
-    } else if (error != discordpp::Client::Error::None) {
-      std::cerr << "❌ Connection Error: "
-                << discordpp::Client::ErrorToString(error)
-                << " - Details: " << errorDetail << std::endl;
-    }
-  });
+        } else if (error != discordpp::Client::Error::None) {
+          std::cerr << "❌ Connection Error: "
+                    << discordpp::Client::ErrorToString(error)
+                    << " - Details: " << errorDetail << std::endl;
+        }
+      });
 
   authenticate(client);
 
@@ -72,36 +74,54 @@ void DiscordClient::authenticate(std::shared_ptr<discordpp::Client> client) {
   args.SetCodeChallenge(codeVerifier.Challenge());
 
   // Begin authentication process
-  client->Authorize(args, [this, client, codeVerifier](auto result, auto code,
-                                                       auto redirectUri) {
-    if (!result.Successful()) {
-      std::cerr << "❌ Authentication Error: " << result.Error() << std::endl;
-      return;
-    } else {
-      std::cout << "✅ Authorization successful! Getting access token...\n";
+  std::fstream file("discord_auth.txt", std::ios::in);
+  std::string accessToken;
+  file >> accessToken;
+  file.close();
 
-      // Exchange auth code for access token
-      client->GetToken(
-          APPLICATION_ID, code, codeVerifier.Verifier(), redirectUri,
-          [client](discordpp::ClientResult result, std::string accessToken,
-                   std::string refreshToken,
-                   discordpp::AuthorizationTokenType tokenType,
-                   int32_t expiresIn, std::string scope) {
-            std::cout
-                << "🔓 Access token received! Establishing connection...\n";
-            client->UpdateToken(
-                discordpp::AuthorizationTokenType::Bearer, accessToken,
-                [client](discordpp::ClientResult result) {
-                  if (result.Successful()) {
-                    std::cout << "🔑 Token updated, connecting to Discord...\n";
-                    client->Connect();
-                  }
-                }
+  if (accessToken.empty()) {
+    client->Authorize(args, [this, client, codeVerifier](auto result, auto code,
+                                                         auto redirectUri) {
+      if (!result.Successful()) {
+        std::cerr << "❌ Authentication Error: " << result.Error() << std::endl;
+        return;
+      } else {
+        std::cout << "✅ Authorization successful! Getting access token...\n";
 
-            );
-          });
-    }
-  });
+        // Exchange auth code for access token
+        client->GetToken(
+            APPLICATION_ID, code, codeVerifier.Verifier(), redirectUri,
+            [client](discordpp::ClientResult result, std::string accessToken,
+                     std::string refreshToken,
+                     discordpp::AuthorizationTokenType tokenType,
+                     int32_t expiresIn, std::string scope) {
+              std::cout
+                  << "🔓 Access token received! Establishing connection...\n";
+              std::fstream file("discord_auth.txt", std::ios::out);
+              file << accessToken << std::endl;
+              file.close();
+            });
+      }
+    });
+    std::fstream file("discord_auth.txt", std::ios::in);
+    file >> accessToken;
+    file.close();
+
+  } else if (!accessToken.empty()) {
+    std::cout << "🔑 Access token found! Using it to connect...\n";
+    client->UpdateToken(
+        discordpp::AuthorizationTokenType::Bearer, accessToken,
+        [client](discordpp::ClientResult result) {
+          if (result.Successful()) {
+            std::cout << "🔑 Token updated, connecting to Discord...\n";
+            client->Connect();
+          } else {
+            std::cerr << "❌ Failed to update token: " << result.Error()
+                      << std::endl;
+          }
+        });
+  }
+  return;
 };
 
 void DiscordClient::SetActivity(std::shared_ptr<discordpp::Client> client,
