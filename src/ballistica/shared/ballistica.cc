73c0d0bead0141d15599ef303e4937b0634c7162
Eric Froemling <ericfroemling@gmail.com>
2026-01-23 10:28:49 -0800

    tidying

--- src/ballistica/shared/ballistica.cc
+++ src/ballistica/shared/ballistica.cc
@@ -44,7 +44,7 @@ auto main(int argc, char** argv) -> int {
 namespace ballistica {
 
 // These are set automatically via script; don't modify them here.
-const int kEngineBuildNumber = 22693;
+const int kEngineBuildNumber = 22696;
 const char* kEngineVersion = "1.7.60";
 const int kEngineApiVersion = 9;
 
