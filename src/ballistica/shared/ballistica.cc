ec62e62bf1965aee23fc423810de74d89287e60b
Eric Froemling <ericfroemling@gmail.com>
2026-06-12 21:45:41 -0700

    Latest public/internal sync.

--- src/ballistica/shared/ballistica.cc
+++ src/ballistica/shared/ballistica.cc
@@ -51,8 +51,8 @@ auto main(int argc, char** argv) -> int {
 namespace ballistica {
 
 // These are set automatically via script; don't modify them here.
-const int kEngineBuildNumber = 22892;
-const char* kEngineVersion = "1.8.0a7";
+const int kEngineBuildNumber = 22893;
+const char* kEngineVersion = "1.8.0a8";
 const int kEngineApiVersion = 9;
 
 #if BA_MONOLITHIC_BUILD
