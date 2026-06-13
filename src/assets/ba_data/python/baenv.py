979b5a0957a356f3736f3e652605e3a65237104e
Eric Froemling <ericfroemling@gmail.com>
2026-06-12 21:00:02 -0700

    Latest public/internal sync.

--- src/assets/ba_data/python/baenv.py
+++ src/assets/ba_data/python/baenv.py
@@ -55,8 +55,8 @@ logger = logging.getLogger('ba.env')
 
 # Build number and version of the ballistica binary we expect to be
 # using.
-TARGET_BALLISTICA_BUILD = 22890
-TARGET_BALLISTICA_VERSION = '1.8.0a6'
+TARGET_BALLISTICA_BUILD = 22892
+TARGET_BALLISTICA_VERSION = '1.8.0a7'
 
 
 @dataclass
