8e1d240eaa789006bc4feea6e2758d14a272c03a
Eric Froemling <ericfroemling@gmail.com>
2026-06-22 10:24:39 -0700

    Remove unused legacy map-data json build step (maps load from python modules)

--- src/ballistica/shared/ballistica.cc
+++ src/ballistica/shared/ballistica.cc
@@ -51,8 +51,8 @@ auto main(int argc, char** argv) -> int {
 namespace ballistica {
 
 // These are set automatically via script; don't modify them here.
-const int kEngineBuildNumber = 22906;
-const char* kEngineVersion = "1.8.0a20";
+const int kEngineBuildNumber = 22907;
+const char* kEngineVersion = "1.8.0a21";
 const int kEngineApiVersion = 9;
 
 #if BA_MONOLITHIC_BUILD
