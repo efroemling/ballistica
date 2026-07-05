1fc6d037384fa1ff55d6eb2641945b817910e81b
Eric Froemling <ericfroemling@gmail.com>
2026-07-04 22:43:31 -0700

    Cache pre-shared zstd dictionaries in the cloud-file codec (one ZstdDict per process instead of one per blob compress/decompress).

--- src/assets/ba_data/python/baenv.py
+++ src/assets/ba_data/python/baenv.py
@@ -55,8 +55,8 @@ logger = logging.getLogger('ba.env')
 
 # Build number and version of the ballistica binary we expect to be
 # using.
-TARGET_BALLISTICA_BUILD = 22923
-TARGET_BALLISTICA_VERSION = '1.8.0a36'
+TARGET_BALLISTICA_BUILD = 22924
+TARGET_BALLISTICA_VERSION = '1.8.0a37'
 
 
 @dataclass
--- src/ballistica/shared/ballistica.cc
+++ src/ballistica/shared/ballistica.cc
@@ -51,8 +51,8 @@ auto main(int argc, char** argv) -> int {
 namespace ballistica {
 
 // These are set automatically via script; don't modify them here.
-const int kEngineBuildNumber = 22923;
-const char* kEngineVersion = "1.8.0a36";
+const int kEngineBuildNumber = 22924;
+const char* kEngineVersion = "1.8.0a37";
 const int kEngineApiVersion = 9;
 
 #if BA_MONOLITHIC_BUILD
