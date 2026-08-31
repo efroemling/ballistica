929520a9a8ffce51e4cd48f4cd65c6df1efe1ccf
Eric Froemling <ericfroemling@gmail.com>
2026-08-31 12:00:51 -0700

    Codegen lazybuild re-runs when its outputs are missing.

--- src/assets/ba_data/python/baenv.py
+++ src/assets/ba_data/python/baenv.py
@@ -55,8 +55,8 @@ logger = logging.getLogger('ba.env')
 
 # Build number and version of the ballistica binary we expect to be
 # using.
-TARGET_BALLISTICA_BUILD = 23009
-TARGET_BALLISTICA_VERSION = '1.8.0a112'
+TARGET_BALLISTICA_BUILD = 23010
+TARGET_BALLISTICA_VERSION = '1.8.0a113'
 
 
 @dataclass
--- src/ballistica/shared/ballistica.cc
+++ src/ballistica/shared/ballistica.cc
@@ -51,8 +51,8 @@ auto main(int argc, char** argv) -> int {
 namespace ballistica {
 
 // These are set automatically via script; don't modify them here.
-const int kEngineBuildNumber = 23009;
-const char* kEngineVersion = "1.8.0a112";
+const int kEngineBuildNumber = 23010;
+const char* kEngineVersion = "1.8.0a113";
 const int kEngineApiVersion = 9;
 
 #if BA_MONOLITHIC_BUILD
